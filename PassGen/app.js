94dd16fca18568f6f1aec0aa868692cb6685c032
unknown <mindaugasskeivys@gmail.com>
2025-05-07 18:05:25 +0300

    first commit

---
+++ PassGen/app.js
@@ -0,0 +1,7 @@
+// Sukurkite password generatorių, kuris leistų vartotojui:
+// Pasirinkit kiek simbolių norės kad būtų slaptažodyje
+// Ar būtų didžiosios raidės
+// Ar būtų skaičiai
+// Ar būtų simboliai
+// Leistų sugeneruotą slaptažodį nusikopijuoti vieno mygtuko paspaudimu
+
